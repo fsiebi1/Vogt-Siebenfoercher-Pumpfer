d578e6aba573f4ce1c5ea18aa46def11f752f7d1
fsiebi1 <fsiebenfoercher@tsn.at>
2021-11-04 11:06:43 +0100

    Initialize

---
+++ CodingLanguages/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using CodingLanguages.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingLanguages.Controllers {
+    public class HomeController : Controller {
+
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
