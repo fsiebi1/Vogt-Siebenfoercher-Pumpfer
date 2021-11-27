8f865ebee20d6d1aeb6fad25e5efb84da5b55b0b
Maximilian-Pumpfer <mpumpfer@tsn.at>
2021-11-27 13:48:07 +0100

    body inhalt

---
+++ CodingLanguages/Controllers/NetController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingLanguages.Controllers
+{
+    public class NetController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult example()
+        {
+            return View();
+        }
+    }
+}
