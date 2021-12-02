db6be199afe1f011a8a2b8e6e9d310c35c1b37a5
fsiebi1 <fsiebenfoercher@tsn.at>
2021-12-02 09:46:09 +0100

    erstellung user

--- CodingLanguages/wwwroot/js/site.js
+++ CodingLanguages/wwwroot/js/site.js
@@ -1,1 +1,28 @@
 ﻿
+function activeNav(x) {
+    if (document.getElementById("Home").classList.contains('active')) {
+        document.getElementById("Home").classList.remove('active');
+    }
+
+    if (document.getElementById("Java").classList.contains('active')) {
+        document.getElementById("Java").classList.remove('active');
+    }
+
+    if (document.getElementById("net").classList.contains('active')) {
+        document.getElementById("net").classList.remove('active');
+    }
+
+    if (document.getElementById("Python").classList.contains('active')) {
+        document.getElementById("Python").classList.remove('active');
+    }
+
+    if (x == 0) {
+        document.getElementById("Home").classList.add('active');
+    } else if (x == 1) {
+        document.getElementById("Java").classList.add('active');
+    } else if (x == 2) {
+        document.getElementById("net").classList.add('active');
+    } else if (x == 3) {
+        document.getElementById("Python").classList.add('active');
+    }
+}
